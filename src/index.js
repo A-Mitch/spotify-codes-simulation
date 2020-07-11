60139cd518c5992d96bd1fa59c1a67e8f56b2df5
Alexander Mitchell <amitchellofficial@gmail.com>
2020-07-10 21:18:05 -0400

    gets color palette array to show up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import './styles/styles.css';
 import coverImg from './images/se_so_neon_non_adaptation.jpg';
 import soundwave from './images/soundwave.png'
+import * as Vibrant from 'node-vibrant'
 
-// const cover = document.querySelector('cover-container')
 
 document.getElementById('album').src = coverImg
+
+const img = document.getElementById('album').src
+
+Vibrant.from(img).getPalette()
+.then((palette) => 
+    
+    console.log(palette)
+    // console.log(`${Vibrant.Util.rgbToHex(165, 177, 212)}`)
+
+
+)
+.catch(err => {console.log(err)})
+
 
 document.getElementById('soundwave_img').src = soundwave
 
 
-console.log("test")
+
+
+
